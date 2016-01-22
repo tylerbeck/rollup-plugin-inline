23be467bceb0eb507f4058f4191957cbc9f5ea32
Tyler Beck <tyler@tylerbeck.com>
2016-01-22 13:53:38 -0500

    add test for write processor

--- src/processors/write.js
+++ src/processors/write.js
@@ -4,13 +4,15 @@ import { verifyInputProperties } from '../util/ProcessorHelpers';
 
 export default function write( options = {}) {
   options.encoding = options.encoding || 'utf8';
+  options.test = options.test || () => true;
 	return {
 		process( obj ) {
       verifyInputProperties( obj, 'contents', 'path' );
       const objPath = obj.path;
       const contents = obj.contents;
       const encoding = options.encoding;
-      if ( contents ) {
+      const path = join( path, objPath );
+      if ( contents  && options.test( path )) {
         obj.write = path => outputFileAsync( join( path, objPath ), contents, encoding );
       }
       return obj;
